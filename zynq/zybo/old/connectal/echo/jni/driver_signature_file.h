074cc843818db3c3f06503b1553752263d2a5374
John Ankcorn <jca@alum.mit.edu>
2018-06-25 20:27:25 -0700

    remove unneeded files

--- zynq/zybo/old/connectal/echo/jni/driver_signature_file.h
+++
@@ -1,6 +0,0 @@
-{"MD5 (/Users/jca/git/connectal/drivers/connectalsdhci/connectalsdhci.c) =", "21c2774a18d73b43c056d6be19659da5"},
-{"MD5 (/Users/jca/git/connectal/drivers/connectalspi/connectalspi.c) =", "a4b8745fca79964a8b75d6cf068c78de"},
-{"MD5 (/Users/jca/git/connectal/drivers/pcieportal/pcieportal.c) =", "47e961a4b445762e6b6024086a3ff16b"},
-{"MD5 (/Users/jca/git/connectal/drivers/portalmem/portalmem.c) =", "d96a232ecd122ef75110250e2d1d1ab4"},
-{"MD5 (/Users/jca/git/connectal/drivers/zynqportal/zynqportal.c) =", "e059016ac411386d6e2597d43bc49e15"},
-{"MD5 (/Users/jca/git/connectal/generated/cpp/MMURequest.c) =", "bd2e0500b4b4ff6fe2eaf90482e237c9"},
